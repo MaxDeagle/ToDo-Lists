a377c24e60d105c4fa99db5c59294f28d0ecb6f9
MaxDeagle <maxjdeagle@gmail.com>
2018-01-31 12:47:17 +0000

    Update task.component.ts

--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -1,1 +1,12 @@
+import { Component } from '@angular/core';
 
+import { Task } from '../task';
+import { ToDoService } from '../todo.service';
+
+@Component({
+  selector: 'task-component',
+  templateUrl: './task.component.html',
+  styleUrls: ['./task.component.css']
+})
+export class TaskComponent {
+}
