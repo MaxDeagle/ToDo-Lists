6183c2e17e318fd447983611d99d82ed9d23cba1
MaxDeagle <maxjdeagle@gmail.com>
2018-02-06 06:26:45 +0000

    Update api.service.ts

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,1 +1,63 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
+
+const httpOptions = {
+  headers: new HttpHeaders({'Accept':'application/json', 'Content-Type':'application/json'})
+};
+
+@Injectable()
+export class AppService {
+
+  constructor(private http: HttpClient) { 
+  }
+
+  get(url): any {
+    this.http.get(url, httpOptions)
+    .toPromise()
+    .then(response => {
+      if (response.success == false) {
+        alert('error!');
+        return null;
+      }
+      return response.data;
+    });
+  }
+  
+  post(url, body): any {
+    this.http.post(url, body, httpOptions)
+    .toPromise()
+    .then(response => {
+      if (respose.success == false {
+        alert('error!');
+        return null;
+      }
+      return response.data;
+    });
+  }
+
+  put(url, body): any {
+    this.http.put(url, body, httpOptions)
+    .toPromise()
+    .then(response => {
+      if (response.success == false {
+        alert('error!');
+        return null;
+      }
+      return response.data;
+    });
+  }
+
+  delete(url): any {
+    this.http.delete(url, httpOptions)
+    .toPromise()
+    .then(response => {
+      if (response.success == false {
+        alert('error!');
+        return null;
+      }
+      return response.data;
+    });
+  }
+  
+}
